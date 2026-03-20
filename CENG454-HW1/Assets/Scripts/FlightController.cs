a581465c6aeec1ca305b8d813e525088499117bc
usbak <usbak28sdal@gmail.com>
2026-03-20 15:43:37 +0300

    feat: add Rigidbody reference for plane

---
+++ CENG454-HW1/Assets/Scripts/FlightController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class flightController : MonoBehaviour
+{  [SerializeField] private float pitchSpeed  = 45f;  // degrees/second 
+    [SerializeField] private float yawSpeed    = 45f;  // degrees/second 
+    [SerializeField] private float rollSpeed   = 45f;  // degrees/second 
+    [SerializeField] private float thrustSpeed = 5f;   // units/second 
+ 
+    private Rigidbody planeRb; 
+ 
+    void Start() 
+    { 
+        // TODO (Task 3-B): Cache GetComponent<Rigidbody>() into 'rb'. 
+        //                  Then set rb.freezeRotation = true. 
+        //                  Why is freezeRotation needed? Answer in your PDF. 
+    } 
+ 
+    void Update()// or FixedUpdate() 
+    { 
+        HandleRotation(); 
+        HandleThrust(); 
+    } 
+ 
+    private void HandleRotation() 
+    { 
+        // TODO (Task 3-C): 
+        // Pitch   
+        // Roll    
+ 
+    } 
+ 
+    private void HandleThrust() 
+    { 
+        // TODO (Task 3-D): 
+    } 
+    
+}
